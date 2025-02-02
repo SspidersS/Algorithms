652a9180c6f96ccc1fb63b04911327850ce44ae3
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-02 13:11:03 +0100

    Delete untitled directory

--- untitled/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Fibonacci.fibonacciIterative(6));
-        System.out.println(Fibonacci.fibonacciRecursive(6));
-        System.out.println(Fibonacci.fibonacciDP(6));
-    }
-}
-
